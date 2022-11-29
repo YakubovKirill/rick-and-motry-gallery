616fc8e82addb019c3af1f85c76a97a839685ac3
Yakubov Kirill <kirill.yakubov@mbicycle.com>
2022-11-29 12:50:26 +0300

    add nav bar element

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import './test.scss';
 
+interface ISomeComponenProps {
+  message: string,
+}
+
+const SomeComponent = ({ message }: ISomeComponenProps) => {
+  return (
+    <>
+      <h1>{ message }</h1>
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <div className="some-class"></div>
+      <div className="nav-bar f-c">
+        <div className="m-w-1200">
+          <SomeComponent message='Hello from some component' />
+        </div>
+      </div>
     </div>
   );
 }
